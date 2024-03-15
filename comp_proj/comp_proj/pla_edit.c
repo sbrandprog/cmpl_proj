7522cd876221534bc5f1119c14f7fdf23570b769
sbrandprog <sbrandprog@gmail.com>
2024-03-15 21:05:50 +0300

    refactor: small improvement on display functions

--- comp_proj/comp_proj/pla_edit.c
+++ comp_proj/comp_proj/pla_edit.c
@@ -29,6 +29,9 @@ typedef struct pla_edit_disp_ctx {
 
 	LONG font_w;
 	LONG font_h;
+
+	size_t cur_x;
+	size_t cur_y;
 } disp_ctx_t;
 
 static bool init_style(style_t * style, pla_edit_style_desc_t * desc) {
@@ -50,47 +53,59 @@ static void cleanup_style(style_t * style) {
 	}
 }
 
-static void disp_spaces(disp_ctx_t * ctx, size_t spaces, size_t * x_ch, size_t * y_ch) {
-	*x_ch += spaces;
+static void disp_spaces(disp_ctx_t * ctx, size_t spaces) {
+	ctx->cur_x += spaces;
 }
-static void disp_newlines(disp_ctx_t * ctx, size_t x_level, size_t lines, size_t * x_ch, size_t * y_ch) {
-	*x_ch = x_level;
-	*y_ch += lines;
+static void disp_newlines(disp_ctx_t * ctx, size_t x_level, size_t lines) {
+	ctx->cur_x = x_level;
+	ctx->cur_y += lines;
 }
-static void disp_text(disp_ctx_t * ctx, pla_edit_col_type_t col, size_t text_size, const wchar_t * text, size_t * x_ch, size_t * y_ch) {
+static void disp_ch(disp_ctx_t * ctx, pla_edit_col_type_t col, wchar_t ch) {
 	SetTextColor(ctx->hdc, ctx->data->style.cols[col].cr);
 
-	TextOutW(ctx->hdc, (int)(*x_ch * ctx->font_w), (int)(*y_ch * ctx->font_h), text, (int)text_size);
+	TextOutW(ctx->hdc, (int)(ctx->cur_x * ctx->font_w), (int)(ctx->cur_y * ctx->font_h), &ch, 1);
+
+	ctx->cur_x += 1;
+}
+static void disp_text(disp_ctx_t * ctx, pla_edit_col_type_t col, size_t text_size, const wchar_t * text) {
+	SetTextColor(ctx->hdc, ctx->data->style.cols[col].cr);
+
+	TextOutW(ctx->hdc, (int)(ctx->cur_x * ctx->font_w), (int)(ctx->cur_y * ctx->font_h), text, (int)text_size);
 	
-	*x_ch += text_size;
+	ctx->cur_x += text_size;
 }
-static void disp_keyw(disp_ctx_t * ctx, pla_keyw_t keyw, size_t * x_ch, size_t * y_ch) {
-	const ul_ros_t * text = &pla_keyw_strs[keyw];
-
-	disp_text(ctx, PlaEditColKeyw, text->size, text->str, x_ch, y_ch);
+static void disp_ros(disp_ctx_t * ctx, pla_edit_col_type_t col, const ul_ros_t * ros) {
+	disp_text(ctx, col, ros->size, ros->str);
 }
-static void disp_punc(disp_ctx_t * ctx, pla_punc_t punc, size_t * x_ch, size_t * y_ch) {
-	const ul_ros_t * text = &pla_punc_strs[punc];
-
-	disp_text(ctx, PlaEditColPlain, text->size, text->str, x_ch, y_ch);
+static void disp_hs(disp_ctx_t * ctx, pla_edit_col_type_t col, ul_hs_t * hs) {
+	disp_text(ctx, col, hs->size, hs->str);
 }
-static void disp_ident(disp_ctx_t * ctx, ul_hs_t * ident, size_t * x_ch, size_t * y_ch) {
-	disp_text(ctx, PlaEditColPlain, ident->size, ident->str, x_ch, y_ch);
+
+static void disp_keyw(disp_ctx_t * ctx, pla_keyw_t keyw) {
+	disp_ros(ctx, PlaEditColKeyw, &pla_keyw_strs[keyw]);
 }
-static void disp_dclr_nspc(disp_ctx_t * ctx, pla_dclr_t * nspc, size_t * x_ch, size_t * y_ch) {
-	size_t x_level = *x_ch;
-
-	disp_keyw(ctx, PlaKeywNamespace, x_ch, y_ch);
-	disp_spaces(ctx, 1, x_ch, y_ch);
-	disp_ident(ctx, nspc->name, x_ch, y_ch);
-	disp_spaces(ctx, 1, x_ch, y_ch);
-	disp_punc(ctx, PlaPuncLeBrace, x_ch, y_ch);
-
-	disp_newlines(ctx, x_level, 2, x_ch, y_ch);
-	disp_punc(ctx, PlaPuncRiBrace, x_ch, y_ch);
-
-	disp_newlines(ctx, x_level, 1, x_ch, y_ch);
+static void disp_punc(disp_ctx_t * ctx, pla_punc_t punc) {
+	disp_ros(ctx, PlaEditColPlain, &pla_punc_strs[punc]);
 }
+static void disp_ident(disp_ctx_t * ctx, ul_hs_t * ident) {
+	disp_hs(ctx, PlaEditColPlain, ident);
+}
+
+static void disp_dclr_nspc(disp_ctx_t * ctx, pla_dclr_t * nspc) {
+	size_t x_level = ctx->cur_x;
+
+	disp_keyw(ctx, PlaKeywNamespace);
+	disp_spaces(ctx, 1);
+	disp_ident(ctx, nspc->name);
+	disp_spaces(ctx, 1);
+	disp_punc(ctx, PlaPuncLeBrace);
+
+	disp_newlines(ctx, x_level, 2);
+	disp_punc(ctx, PlaPuncRiBrace);
+
+	disp_newlines(ctx, x_level, 1);
+}
+
 static void disp_test(wnd_data_t * data, HDC hdc) {
 	disp_ctx_t ctx = { .data = data, hdc = hdc };
 
@@ -107,16 +122,14 @@ static void disp_test(wnd_data_t * data, HDC hdc) {
 
 		GetTextMetricsW(hdc, &tm);
 
-		ctx.font_h = tm.tmAscent;
+		ctx.font_h = tm.tmHeight;
 		ctx.font_w = tm.tmAveCharWidth;
 	}
 	
 	{
-		size_t x_ch = 0, y_ch = 0;
-
 		pla_dclr_t nspc_example = { .type = PlaDclrNspc, .name = UL_HST_HASHADD_WS(data->ctx->hst, L"pla_lib") };
 
-		disp_dclr_nspc(&ctx, &nspc_example, &x_ch, &y_ch);
+		disp_dclr_nspc(&ctx, &nspc_example);
 	}
 }
 
@@ -241,7 +254,7 @@ pla_edit_style_desc_t pla_edit_get_style_desc_dflt(wa_ctx_t * ctx) {
 		.font = ctx->style.fonts[WaStyleFontFxd].lf
 	};
 
-	desc.font.lfHeight = -14;
+	desc.font.lfHeight = -16;
 
 	return desc;
 }
